dede9d22c665674c4fa4fa6b4b9803a496868e3f
Ivan-R-BS <103216417+Ivan-R-BS@users.noreply.github.com>
2022-04-21 13:36:33 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,30 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.util.ArrayList;
 import org.junit.*;
 
-public class MarkdownParseTest {
+public class MarkdownParseTest
+{
     @Test
-public void addition() {
-assertEquals(2, 1 + 1);
-}
+    public void addition() 
+    {
+    assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void linktest() throws IOException
+    {
+        ArrayList<String> test1= new ArrayList<>();
+        test1.add("https://something.com");
+        test1.add("some-thing.html");
+        String tss = "test-file.md";
+        Path fileName = Path.of(tss);
+        String content = Files.readString(fileName);
+        assertEquals(test1,MarkdownParse.getLinks(content));
+    }
+
+
+
 }
